334200535f949da50a7dfb3822caea37dba573d7
BantzInThePantz <adamoboman@gmail.com>
2025-12-11 17:06:57 +0100

    Correct prompt shows up based on interaction

--- Assets/Scripts/InteractionType.cs
+++ Assets/Scripts/InteractionType.cs
@@ -2,6 +2,8 @@ public enum InteractionType
 {
     None   = 0,
     Pickup = 1,
-    NPC    = 2,
-    // you can add more later, higher number = higher priority
+    Door   = 2,
+    NPC    = 3,
+
+    // add more, high numbers = higher priority
 }
