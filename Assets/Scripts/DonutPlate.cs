4615f58c281f22c2267169aa23e37bce193c933a
zbellers <44782337+zbellers@users.noreply.github.com>
2025-12-12 01:56:10 +0100

    Start In the kitchen
    The player now starts in the kitchen looking at an empty plate.

---
+++ Assets/Scripts/DonutPlate.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class DonutPlate : MonoBehaviour
+{
+    public GameObject panel;
+    //private bool messageShown = false;    // Ensures it only shows once
+
+    void Start()
+    {
+        //if (panel != null)
+          //  panel.SetActive(false); // ensure panel is hidden at start
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Trigger Entered by: " + other.name);
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player entered the donut plate area.");
+            panel.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Trigger Exited by: " + other.name);
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player exited the donut plate area.");
+            panel.SetActive(false);
+        }
+    }
+
+}
